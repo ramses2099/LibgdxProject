dd79670ee015a61760cdceb21ef3339f676a4ea6
ramses2099 <ramses2099@gmail.com>
2024-02-03 21:56:35 -0400

    commit all

---
+++ core/src/com/jprograming/LibgdxProject.java
@@ -0,0 +1,32 @@
+package com.jprograming;
+
+import com.badlogic.gdx.Game;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.jprograming.screen.MainMenuScreen;
+
+public class LibgdxProject extends Game {
+    public SpriteBatch batch;
+    public BitmapFont font;
+
+
+    @Override
+    public void create() {
+        batch = new SpriteBatch();
+        font = new BitmapFont();
+        this.setScreen(new MainMenuScreen(this));
+    }
+
+    @Override
+    public void render() {
+        super.render();
+    }
+
+    @Override
+    public void dispose() {
+        batch.dispose();
+        font.dispose();
+    }
+
+
+}
